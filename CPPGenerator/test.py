1c626ee6da7a1bf2352b6c8a403fdd3b2f46162e
KerryChen <52032167+QiufenChen@users.noreply.github.com>
2025-03-18 23:47:33 +0800

    Update test.py

--- CPPGenerator/test.py
+++ CPPGenerator/test.py
@@ -21,7 +21,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Initialize model and tokenizer
-model_name = "../Rostlab/prot_bert_bfd"
+model_name = "../Rostlab/prot_bert"
 device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 tokenizer = BertTokenizer.from_pretrained(model_name)
 
